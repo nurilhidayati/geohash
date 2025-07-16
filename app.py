65188b8656bd2e49fbc817249ecc08b8467d7eff
Nuril Hidayati <nurilassajdah@gmail.com>
2025-07-17 01:06:17 +0700

    cek

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
+
 import streamlit as st
 
-st.set_page_config(page_title="GeoHash Toolbox", page_icon="🌍")
+st.title("Geohash Converter")
 
-st.title("🌍 GeoHash Toolbox")
-st.markdown("""
-Selamat datang di **GeoHash Toolbox**!
+st.markdown("You can obtain geohash using this tools by simply copy coordinates or drawing polygon using this tools and download it as csv files ")
 
-Gunakan sidebar di kiri untuk mengakses fitur:
-- 🔄 Konversi Geohash ke GeoJSON
-- ➡️ Konversi GeoJSON ke Geohash
-- 🗺️ Viewer Peta GeoJSON
+st.header("1. Copy Coordinates")
+st.write(" If you have list of coordinates separated by comma, you can just paste it in fill column and the choose which digit geohash you want then you can download it as csv")
 
-Website ini membantu kamu dalam mengolah data spasial dengan format geohash secara praktis.
-""")
+
+st.video(video_bytes1)
+
+st.header("2. Draw Polygon")
+st.write("Geohash can be obtained by draw polygon over area you want to obtain geohash, then pick digit geohash you want, after that another map will popup show geohash coverage based your polygon drawing. ")
+
