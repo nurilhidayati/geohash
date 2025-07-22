dc69d1d87746647e74d038b0b638993b38cee706
Nuril Hidayati <nurilassajdah@gmail.com>
2025-07-22 20:07:51 +0700

    update

--- pages/0_Download_geojson.py
+++ pages/0_Download_geojson.py
@@ -99,7 +99,7 @@ st.markdown(
     """
     <hr style="margin-top: 2rem; margin-bottom: 1rem;">
     <div style='text-align: center; color: grey; font-size: 0.9rem;'>
-        © 2025 Karta IoT Team
+        © 2025 ID Karta IoT Team
     </div>
     """,
     unsafe_allow_html=True
